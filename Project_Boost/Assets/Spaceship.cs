621e274d1e794d3bea5bd04f1684996cf0edb00c
fjsch <fjschuetze@gmail.com>
2019-09-01 22:11:04 +0900

    Added relative force on space bar press.

---
+++ Project_Boost/Assets/Spaceship.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput(); 
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate left");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            print("Rotate right");
+        }
+
+    }
+}
